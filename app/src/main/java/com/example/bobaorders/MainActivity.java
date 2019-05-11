bcd54d41bd1568d62f90e0babf115352e8acd8d6
Aidan Lee <meow123al@gmail.com>
2019-05-11 00:44:47 -0700

    Expanded Main Activity, Added Retrieval Activity
    Added Buttons and ImageView to MainActivity.
    Created new RetrievalActivity.
    Added Link from MainActivity to RetrievalActivity via Button.

--- app/src/main/java/com/example/bobaorders/MainActivity.java
+++ app/src/main/java/com/example/bobaorders/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.bobaorders;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView img = (ImageView) findViewById(R.id.bobalogo);
+//        getScaleLogo(img);
+
+        Button btnOrder = (Button) findViewById(R.id.btnOrder);
+        Button btnRetrieve = (Button) findViewById(R.id.btnRetrieve);
+
+        btnRetrieve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToRetrievalActivity = new Intent(getApplicationContext(), RetrievalActivity.class);
+                startActivity(goToRetrievalActivity);
+            }
+        });
+
+
+
+
+    }
+
+    private void getScaleLogo(ImageView img){
+
+        int logo = R.drawable.bobalogo;
+        Display screen = getWindowManager().getDefaultDisplay();
+        BitmapFactory.Options options= new BitmapFactory.Options();
+
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(), logo, options);
+
+        int imgWidth = options.outWidth;
+        int screenWidth = screen.getWidth();
+
+        if(imgWidth > screenWidth){
+            int ratio = Math.round((float) imgWidth / (float) screenWidth);
+            options.inSampleSize = ratio;
+        }
+
+        options.inJustDecodeBounds = false;
+        Bitmap scaledImg = BitmapFactory.decodeResource(getResources(), logo, options);
+        img.setImageBitmap(scaledImg);
+
+
+
     }
 }
