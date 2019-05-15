c9f7f3235bff6c852ceb0a51cb7380571791b5dc
Aidan Lee <meow123al@gmail.com>
2019-05-14 22:28:30 -0700

    New Branch: Contains WIP firebase RecycleViewAdapter

---
+++ app/src/main/java/com/example/bobaorders/MenuDisplayBind.java
@@ -0,0 +1,27 @@
+package com.example.bobaorders;
+
+public class MenuItem {
+    private String name;
+    private int price;
+
+    public MenuItem(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
